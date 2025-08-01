b62b1e09301f08d7f89b2ab756579c2e4f56c177
Soroush <soroushbahramiandev@gmail.com>
2025-08-01 20:44:34 +0330

    Show the genres loading skeletons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,17 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data , isLoading , error } = useGenres();
+  const skeletons = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+  if (error) return null;
+
   return (
     <List>
+      {isLoading && skeletons.map((skeleton)=> <GenreListSkeleton key={skeleton}/>)}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY='5px'>
           <HStack>
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,18 @@
+import {
+  HStack,
+  Skeleton,
+  VStack,
+} from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  return (
+    <VStack align="stretch" py="5px">
+      <HStack spacing="7px">
+        <Skeleton boxSize="32px" borderRadius={8} overflow="hidden" />
+        <Skeleton height="18px" width="90px" borderRadius="5px" />
+      </HStack>
+    </VStack>
+  );
+};
+
+export default GenreListSkeleton;
